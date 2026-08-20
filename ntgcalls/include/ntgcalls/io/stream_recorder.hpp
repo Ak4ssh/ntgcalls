86169a0b7051686264e09431ddd97c7efe2b0e8e
Akash Tiwari <84653553+Ak4ssh@users.noreply.github.com>
2026-08-20 21:02:32 +0530

    Implement StreamRecorder class
    Added StreamRecorder class for recording streams with thread safety.

--- ntgcalls/include/ntgcalls/io/stream_recorder.hpp
+++ ntgcalls/include/ntgcalls/io/stream_recorder.hpp
@@ -1,1 +1,22 @@
 
+#pragma once
+#include <mutex>
+#include <fstream>
+#include <ntgcalls/media/base_sink.hpp>
+#include <wrtc/utils/binary.hpp>
+
+namespace ntgcalls {
+    class StreamRecorder {
+    public:
+        StreamRecorder();
+        ~StreamRecorder();
+        void start(const std::string& path);
+        void stop();
+        [[nodiscard]] bool isRecording() const;
+        void feedFrame(const bytes::unique_binary& data, size_t size);
+    private:
+        bool is_recording = false;
+        std::ofstream output;
+        mutable std::mutex mutex;
+    };
+}
